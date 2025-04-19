8eacef4cc63a4f763554adc0b4d1059cf5e71574
AmanSIngh494 <amanforwork1@gmail.com>
2025-04-19 16:47:41 +0530

    Backend init

---
+++ backend/app.js
@@ -0,0 +1,26 @@
+import express from 'express'
+import userRoutes from './src/routes/user.js'
+import dotenv from 'dotenv'
+import cors from 'cors'
+
+dotenv.config()
+
+const app = express()
+
+app.use(
+  cors({
+    origin: [
+      'http://localhost:5173',
+      'http://192.168.110.180:5173',
+      'https://edu-gamix.vercel.app'
+    ],
+    credentials: true // Allows cookies/auth headers
+  })
+)
+app.use(express.json())
+app.use('/user', userRoutes)
+
+const PORT = process.env.PORT || 3000
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`)
+})
